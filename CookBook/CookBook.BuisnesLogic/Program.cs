0d75ca239e65b80d129abcfff1c152d6b93eb213
Tomasz <bienkowski.it@gmail.com>
2024-01-14 20:12:22 +0100

    added CookBook solution

---
+++ CookBook/CookBook.BuisnesLogic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CookBook.BuisnesLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
