2154a32f87f647f7cca92914f4b1a6dffdbb012f
Marek Pastwa <marek@markowo.pl>
2024-01-16 21:59:52 +0100

    menu v1 created

---
+++ CookBook/CookBook.UI/Menu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.UI
+{
+    public class Menu
+    {
+        public int CurrentMenuPosition { get; set; }
+        private string _title { get; set; }
+        private List<string> MenuNameList = new List<string>();
+        private List<string> MenuActionList = new List<string>();
+
+        public Menu()
+        {
+            CurrentMenuPosition = 0;
+        }
+        private enum Actions
+        {
+            login,
+            logout,
+            guestmenu,
+            usermenu,
+            receipeshow
+        }
+
+        public void Title(string title)
+        {
+            _title = title;
+        }
+        public void AddPosition(string positionName, string action)
+        {
+            if (Enum.IsDefined(typeof(Actions), action))
+            {
+                MenuNameList.Add(positionName);
+                MenuActionList.Add(action);
+            }
+        }
+
+        public void ShowMenu()
+        {
+            var x = 0;
+
+            Console.Clear();
+            Console.WriteLine(_title);
+            Console.BackgroundColor = ConsoleColor.Black;
+            for (var i=0; i < MenuNameList.Count; i++)
+            {
+                if (i == CurrentMenuPosition) Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"{++x}. {MenuNameList[i]}");
+                if (i == CurrentMenuPosition) Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+
+        public void OptionSelect()
+        {
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (CurrentMenuPosition != 0)
+                    {
+                        CurrentMenuPosition--;
+                        ShowMenu();
+                    }
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (CurrentMenuPosition != MenuNameList.Count - 1)
+                    {
+                        CurrentMenuPosition++;
+                        ShowMenu();
+                    }
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                    break;
+            } while (true);
+
+        }
+    }
+}
--- CookBook/CookBook.UI/Program.cs
+++ CookBook/CookBook.UI/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu loginMenu = new Menu();
+            loginMenu.Title("Menu logowania:");
+            loginMenu.AddPosition("Login", "login");
+            loginMenu.AddPosition("Zaloguj się jako gość", "guestmenu");
+            loginMenu.AddPosition("Zakończ", "logout");
+
+            loginMenu.ShowMenu();
+            loginMenu.OptionSelect();
+
         }
     }
 }
