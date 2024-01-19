31a13c1b0cc1e256fe5fb15d8cbd6f0ab5d2da95
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-01-19 19:57:34 +0100

    Add Recipe.cs

---
+++ CookBook/CookBook.BuisnesLogic/Models/Recipe.cs
@@ -0,0 +1,38 @@
+﻿
+namespace CookBook.BuisnesLogic.Models
+{
+    internal class Recipe
+    {
+        private string name;
+        private string description;
+        private double rating = 0;
+        private List<double> scoreList = new List<double>();
+
+        public Recipe(string recipeName, string recipeDescription, double recipeScore)
+        {
+            name = recipeName;
+            description = recipeDescription;
+            RateRecipe(recipeScore);
+        }
+        public Recipe(string recipeName, string recipeDescription)
+        {
+            name = recipeName;
+            description = recipeDescription;
+        }
+
+        public void RateRecipe(double rating)
+        {
+            scoreList.Add(rating);
+            CountRating();
+        }
+        private void CountRating()
+        {
+            rating = scoreList.Sum() / scoreList.Count;
+        }
+
+        public override string ToString()
+        {
+            return $"{name}\n{description}\nOcena: {rating:0.##}.";
+        }
+    }
+}
