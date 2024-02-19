e667dda26452f41c1168b05fc63c8f0d0904fea8
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-02-19 11:58:05 +0100

    Add app basic controllers and views files/folders

---
+++ CookBook/AionCodeMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AionCodeMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
