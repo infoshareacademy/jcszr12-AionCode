6c79f18ba054c3ecaf5f87f882d8e6ad08983369
Andrzej Bednarz <andrzejbednarzj23@gmail.com>
2024-03-07 19:54:06 +0100

    Chenged change layout update bootstrap's

--- CookBook/AionCodeMVC/Controllers/RecipesController.cs
+++ CookBook/AionCodeMVC/Controllers/RecipesController.cs
@@ -6,7 +6,11 @@ namespace AionCodeMVC.Controllers
     {
         public IActionResult Index()
         {
+            //TempData["Active"] = 2;
+            ViewBag.quantity = 2;
+
             return View();
         }
+      
     }
 }
