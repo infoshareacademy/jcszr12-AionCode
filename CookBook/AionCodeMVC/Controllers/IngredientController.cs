129392f1be28678546fba3e8d1936169fbe7dca9
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-03-12 10:16:48 +0100

    add IngredientController

---
+++ CookBook/AionCodeMVC/Controllers/IngredientController.cs
@@ -0,0 +1,93 @@
+﻿using AionCodeMVC.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AionCodeMVC.Controllers
+{
+    public class IngredientController : Controller
+    {
+
+        private IIngredientService _ingredientService;
+        public IngredientController(IIngredientService ingredientService)
+        {
+                _ingredientService = ingredientService;
+        }
+
+
+        // GET: IngredientController
+        public ActionResult Index()
+        {
+            var allIngredients = _ingredientService.GetAll();
+            return View(allIngredients);
+        }
+
+        // GET: IngredientController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: IngredientController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IngredientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IngredientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: IngredientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IngredientController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: IngredientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
