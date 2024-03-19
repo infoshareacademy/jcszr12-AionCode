df5b7f1247e23d80db0efca3868b3c0306d192f5
tomasz-bienkowski <150851663+tomasz-bienkowski@users.noreply.github.com>
2024-03-19 08:17:01 +0100

    added test view for adding file

---
+++ CookBook/AionCodeMVC/Controllers/UploadFileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AionCodeMVC.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        public UploadFileController()
+        {
+        }
+        public IActionResult Index() 
+        {
+            return View();
+        }
+        [HttpPost] 
+        public IActionResult Upload(IFormFile file)
+        {
+            return RedirectToAction(nameof(Index));            
+        }
+    }
+}
