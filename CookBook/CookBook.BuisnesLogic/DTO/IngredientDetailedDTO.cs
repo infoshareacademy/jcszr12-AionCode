e295feeb1135e74057f2f64d1c3f08f66719fc8b
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-04-14 17:23:43 +0200

    add Ingredient controller views improvements for better visuals

--- CookBook/CookBook.BuisnesLogic/DTO/IngredientDetailedDTO.cs
+++ CookBook/CookBook.BuisnesLogic/DTO/IngredientDetailedDTO.cs
@@ -13,14 +13,22 @@ namespace CookBook.BuisnesLogic.DTO
 {
     public class IngredientDetailedDTO
     {
+        [DisplayName("Nazwa")]
         public string Name { get; set; }
+        [DisplayName("Opis")]
         public string Description { get; set; }
+        [DisplayName("Typ")]
         public string Type { get; set; }
+        [DisplayName("Kalorie [100g]")]
         public decimal Calories { get; set; }
+        [DisplayName("Białko [g]")]
         public decimal Proteins { get; set; }
+        [DisplayName("Tłuszcz [g]")]
         public decimal Fats { get; set; }
+        [DisplayName("Węglowodany [g]")]
         public decimal Carbohydrates { get; set; }
         public string? ImagePath { get; set; }
+        [DisplayName("Indeks Glikemiczny")]
         public int GI { get; set; }
     }
 }
--- CookBook/SetupInfo/Ingredient/przykladowy.sql
+++ CookBook/SetupInfo/Ingredient/przykladowy.sql
@@ -25,6 +25,8 @@ INSERT INTO [dbo].[IngredientDetails]
            ,[GI]
            ,[UserCookBookId])
      VALUES
-           ('Czereśnia','Słodka z pestką','Owoc',61,1,0.3,14.6,'http://127.0.0.1:10000/devstoreaccount1/ingredient-files/czeresnia.png','2024-08-12',22,1),
-		   ('Kapusta','Zielona z liściami','Warzywo',38,1.7,0.2,7.4,'http://127.0.0.1:10000/devstoreaccount1/ingredient-files/kapusta.png','2024-08-12',15,1),
-		   ('Pietruszka','Zielona z liściami','Warzywo',38,2.6,0.5,15.6,'http://127.0.0.1:10000/devstoreaccount1/ingredient-files/pietruszka.png','2024-08-12',11,1);
+           ('Czereśnia','Słodka z pestką','Owoc',61,1,0.3,14.6,'czeresnia.png','2024-08-12',22,1),
+		   ('Kapusta','Zielona z liściami','Warzywo',38,1.7,0.2,7.4,'kapusta.png','2024-08-12',15,1),
+		   ('Pietruszka','Zielona z liściami','Warzywo',38,2.6,0.5,15.6,'pietruszka.png','2024-08-12',11,1),
+		   ('Kalafior','Kalafior zaliczany jest do cenniejszych warzyw z uwagi na swój skład chemiczny, a także walory smakowe i dietetyczne. Zawiera m.in.: sód, potas, magnez, wapń, mangan, żelazo, miedź, cynk, fosfor, fluor, chlor, jod, karoteny, witaminy:  K, B1, B2, B6, C; kwasy: nikotynowy i pantotenowy.','Warzywo',25.00,2.6,0.5,15.6,'kalafior.png','2024-04-14',16,1);
+
