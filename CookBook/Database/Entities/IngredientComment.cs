025e833c1c3b864ffcfa53e805ff62c88c129695
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-05-27 08:54:22 +0200

    add MyFride, MyFridgeIngredient table to db, remove smalldate from tables

---
+++ CookBook/Database/Entities/IngredientComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Entities
+{
+    public class IngredientComment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+
+        [ForeignKey("IngredientDetails")]
+        public int IngredientDetailsId { get; set; }
+        public virtual IngredientDetails IngredientDetails { get; set; }
+    }
+}
