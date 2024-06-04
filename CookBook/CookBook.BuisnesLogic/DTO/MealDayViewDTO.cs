b2f3de4bf23dcd95095ac9065bd3d6cca41795f4
Andrzej Bednarz <andrzejbednarzj23@gmail.com>
2024-06-04 18:46:07 +0200

    mealdayvievDTO

---
+++ CookBook/CookBook.BuisnesLogic/DTO/MealDayViewDTO.cs
@@ -0,0 +1,15 @@
+﻿using Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.BuisnesLogic.DTO
+{
+    public class MealDayViewDTO
+    {
+   public List<MealDay> MealDaysList { get; set; }
+       
+    }
+}
