d0e57381693c162c113953656904115d57a8237e
Andrzej Bednarz <andrzejbednarzj23@gmail.com>
2024-06-07 20:49:00 +0200

    Updated HomeController

--- CookBook/AionCodeMVC/Controllers/HomeController.cs
+++ CookBook/AionCodeMVC/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace AionCodeMVC.Controllers
 
         public IActionResult Index()
         {
-            var result = _context.RecipeDetails.Where(x => x.Id == 1).FirstOrDefault();
+            var result = _context.RecipeDetails.Where(x => x.Id == 3).FirstOrDefault();
             ViewBag.UserName = User.Identity.Name;
             
             return View(result);
